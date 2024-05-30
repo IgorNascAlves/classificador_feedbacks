82c3493815f9cd40fe4d9a9740b96072210d62d2
Igor do Nascimento Alves <igor.nascimento.flipe@gmail.com>
2024-05-29 23:26:20 -0300

    comentários no arquivo run.py explicando modo debug

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import create_app  # Importa a função create_app do módulo app
 
+# Cria uma instância do aplicativo utilizando a função create_app
 app = create_app()
 
+# Verifica se este arquivo é executado como o programa principal
 if __name__ == "__main__":
+    # Inicia o servidor Flask em modo de depuração se este arquivo for o programa principal
     app.run(debug=True)
